f839bbb7165a01f11e251bbaed87eb8eaf78cf80
calii2008 <ensarsahiti60@gmail.com>
2026-05-12 16:10:09 +0200

    5

---
+++ main.screen.js
@@ -0,0 +1,37 @@
+import react from "react";
+import { ImageBackground } from "react-native-web";
+import { text , stylesheet , View } from "react-neative";
+
+const mainscreen =() => {
+  return(
+     (
+        <view>
+            <text>this is the main screen</text>
+        </view>
+     )
+
+  )
+
+}
+
+
+const style = stylesheet.create({
+
+    container: {
+        flex: 1,
+        ImageBackgroundcolor: ""
+        aligintems: 'center'
+
+    }
+     textstyle: {
+        fontsize: 30
+     }
+}
+export default mainscreen;
+
+
+
+
+
+
+)
