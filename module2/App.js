807a8bdcc8d73f9e132e1b40685b5c86e8293228
calii2008 <ensarsahiti60@gmail.com>
2026-05-14 16:10:43 +0200

    3

---
+++ module2/App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer } from '@react-navigation/native';
+import{ createNativeStackNavigator } from '@react-navigation/native-stack';
+import MainScreen from './screens/MainScreen';
+
+const stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+   <NavigationContainer>
+     <Stack.Navigator>
+      <Stack.Screen name='Home' component={MainScreen}></Stack.Screen>
+     </Stack.Navigator>
+   </NavigationContainer>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ module2/screens/MainScreen.js
@@ -0,0 +1,24 @@
+import react from "react";
+import { Text } from "react-native-web";
+import { StyleSheet, Text, View } from 'react-native';
+
+
+const Mainscreen = () => {
+    return (
+
+
+            <Text>Mainscreen</Text>
+
+    );
+    
+
+}
+
+
+const styles = StyleSheet.create({
+    textStyle: {
+        fontSize: 30
+    }
+});
+
+export default Mainscreen;
