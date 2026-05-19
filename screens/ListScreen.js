4fb2d06d84a3323cb4d3b70756d7bc9d91155841
calii2008 <ensarsahiti60@gmail.com>
2026-05-19 16:16:16 +0200

    a

---
+++ screens/ListScreen.js
@@ -0,0 +1,19 @@
+import react from "react";
+import { text, StyleSheet, view, flatklis } from "react-native-web":
+
+const students = [
+    {name: 'ensar' , surname: 'Sahiti', age : '18'},
+     {name: 'shpati' , surname: 'berisha', age : '16'},
+    {name: 'olsa' , surname: 'pireva', age : '16'},
+    {name: 'daris' , surname: 'ahmeti', age : '18'},
+
+];
+
+
+const listScreen = () =>
+    return(
+        <view></view>
+
+
+
+    )
